893432e65a67dcbbb226d8e67261cac064175cdc
Matt Russo <matthew.russo91@gmail.com>
2025-11-13 21:21:03 -0500

    add initial homepage and styles

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,9 @@
 import { Text, View } from "react-native";
 
-import './global.css';
-
-export default function App() {
+export default function Index() {
   return (
-    <>
-      <View className="flex-1 justify-center items-center">
+    <View className="flex-1 justify-center items-center">
       <Text className="text-5xl text-blue-500">Welcome!</Text>
     </View>
-    </>
   );
 }
